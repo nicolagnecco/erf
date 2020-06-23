67d466ab99c2874443a041be403aede156d826df
Nicola <nicola.gnecco@gmail.com>
2020-06-23 14:54:06 +0200

    Set up main function

--- R/extremeForest.R
+++ R/extremeForest.R
@@ -1,78 +1,120 @@
 #' ExtremeForest
 #'
 #' Computes quantile extreme forest ...
-extreme_forest <- function(X.train, Y, X.test, alpha, alpha.new, Y.test=NULL, grf=FALSE){
+#' @param X.train ...
+#' @param Y ...
+#' @param X.test ...
+#' @param alpha ...
+#' @param alpha.new ...
+#' @param min.node.size ...
+#' @param Y.test ...
+#' @param grf ...
+#' @return ...
+#'
+#' @export
+extreme_forest <- function(X.train, Y, X.test, alpha, alpha.new, min.node.size =5,
+                           Y.test=NULL, grf=FALSE){
 
   ntest <- nrow(X.test)
   results <- array(NA, dim = c(ntest, length(alpha.new)))
   EVT.par <- array(NA, dim = c(ntest, 2))
 
-  fit.grf = quantile_forest(X.train, Y, quantiles = c(0.1,0.5,0.9), min.node.size = min.node.size) #c(0.7, 0.8, alpha, 0.95)
-  q.hat.train = predict(fit.grf, X.train, quantiles = alpha)
+  fit.grf = grf::quantile_forest(X.train, Y, quantiles = c(0.1,0.5,0.9),
+                                 min.node.size = min.node.size) #c(0.7, 0.8, alpha, 0.95)
+  q.hat.train = stats::predict(fit.grf, X.train, quantiles = alpha)
   exc.idx = which(Y - q.hat.train > 0)
   exc.data = (Y - q.hat.train)[exc.idx]
 
 
   # Estimate GRF and EVT quantiles
-  if(grf) results.grf <- predict(fit.grf, X.test, quantiles = alpha.new)
+  if (grf){
+    results.grf <- stats::predict(fit.grf, X.test, quantiles = alpha.new)
+  }
 
 
 
   # Obtaining weights (dimension is ntest x n)
-  qrf.weights = get_sample_weights(fit.grf, newdata = X.test, num.threads = NULL)
+  qrf.weights = grf::get_sample_weights(fit.grf, newdata = X.test,
+                                        num.threads = NULL)
 
 
   #plot(X.train[,1],qrf.weights[24,])
 
-  in.par <- gpd.fit(exc.data, 0, show=FALSE)$mle
+  in.par <- ismev::gpd.fit(exc.data, 0, show=FALSE)$mle
   for(i in 1:ntest){
-    EVT.par[i,] = optim(par=in.par, fn=weighted.LLH, data=exc.data, weights=qrf.weights[i,exc.idx])$par
+    EVT.par[i,] = stats::optim(par=in.par, fn=weighted.LLH, data=exc.data,
+                               weights=qrf.weights[i,exc.idx])$par
   }
   #plot(X.test[,1],EVT.par[,1,r])
 
   # Estimate GRF and EVT quantiles
-  q.alpha.test = predict(fit.grf, X.test, quantiles = alpha)
+  q.alpha.test = stats::predict(fit.grf, X.test, quantiles = alpha)
   for(j in 1:length(alpha.new)){
-    results[,j] <- q.GPD(q = alpha.new[j], alpha=alpha, u = q.alpha.test, sigma = EVT.par[,1], xi = EVT.par[,2])
+    results[,j] <- q.GPD(q = alpha.new[j], alpha=alpha,
+                         u = q.alpha.test, sigma = EVT.par[,1], xi = EVT.par[,2])
   }
 
-  if(!is.null(Y.test)){
-    par(mfrow=c(1,3))
+  # if(!is.null(Y.test)){
+  #   par(mfrow=c(1,3))
+  #
+  #   ## plot qq plot for unconditional model, ignoring covariates
+  #   q.hat.uncon <- quantile(Y, probs = alpha)
+  #   exc.uncon.idx = which(Y - q.hat.uncon > 0)
+  #   exc.uncon = (Y - q.hat.uncon)[exc.uncon.idx]
+  #   uncon.par <- gpd.fit(exc.uncon, 0, show=FALSE)$mle
+  #
+  #   exc.test.uncon.idx = which(Y.test -  q.hat.uncon > 0)
+  #   exc.test.uncon = (Y.test - q.hat.uncon)[exc.test.uncon.idx]
+  #   n.exc.test.uncon = length(exc.test.uncon)
+  #   exc.test.uncon.std <- 1/uncon.par[2] * log(1+uncon.par[2] * exc.test.uncon/uncon.par[1])
+  #   plot(qexp((1:n.exc.test.uncon)/ (n.exc.test.uncon+1)), sort(exc.test.uncon.std))
+  #   abline(0,1)
+  #
+  #   ## plot qq plot for model that uses covariates only for the alpha-threshold
+  #   exc.test.idx = which(Y.test -  q.alpha.test > 0)
+  #   exc.test = (Y.test - q.alpha.test)[exc.test.idx]
+  #   n.exc.test = length(exc.test)
+  #
+  #   exc.test.std2 <- 1/in.par[2] * log(1+in.par[2] * exc.test/in.par[1])
+  #   plot(qexp((1:n.exc.test)/ (n.exc.test+1)), sort(exc.test.std2))
+  #   abline(0,1)
+  #
+  #
+  #   ## plot qq plot for full covariate model for alpha-threshold and GPD parameters
+  #   exc.test.std <- 1/EVT.par[exc.test.idx,2] * log(1+EVT.par[exc.test.idx,2] * exc.test/EVT.par[exc.test.idx,1])
+  #   plot(qexp((1:n.exc.test)/ (n.exc.test+1)), sort(exc.test.std))
+  # abline(0,1)
+  #
+  #
+  #
+  # }
 
-    ## plot qq plot for unconditional model, ignoring covariates
-    q.hat.uncon <- quantile(Y, probs = alpha)
-    exc.uncon.idx = which(Y - q.hat.uncon > 0)
-    exc.uncon = (Y - q.hat.uncon)[exc.uncon.idx]
-    uncon.par <- gpd.fit(exc.uncon, 0, show=FALSE)$mle
-
-    exc.test.uncon.idx = which(Y.test -  q.hat.uncon > 0)
-    exc.test.uncon = (Y.test - q.hat.uncon)[exc.test.uncon.idx]
-    n.exc.test.uncon = length(exc.test.uncon)
-    exc.test.uncon.std <- 1/uncon.par[2] * log(1+uncon.par[2] * exc.test.uncon/uncon.par[1])
-    plot(qexp((1:n.exc.test.uncon)/ (n.exc.test.uncon+1)), sort(exc.test.uncon.std))
-    abline(0,1)
-
-    ## plot qq plot for model that uses covariates only for the alpha-threshold
-    exc.test.idx = which(Y.test -  q.alpha.test > 0)
-    exc.test = (Y.test - q.alpha.test)[exc.test.idx]
-    n.exc.test = length(exc.test)
-
-    exc.test.std2 <- 1/in.par[2] * log(1+in.par[2] * exc.test/in.par[1])
-    plot(qexp((1:n.exc.test)/ (n.exc.test+1)), sort(exc.test.std2))
-    abline(0,1)
-
-
-    ## plot qq plot for full covariate model for alpha-threshold and GPD parameters
-    exc.test.std <- 1/EVT.par[exc.test.idx,2] * log(1+EVT.par[exc.test.idx,2] * exc.test/EVT.par[exc.test.idx,1])
-    plot(qexp((1:n.exc.test)/ (n.exc.test+1)), sort(exc.test.std))
-    abline(0,1)
-
-
-
+  if (!grf){
+    return(list(results=results, EVT.par=EVT.par, X.train=X.train,
+                Y=Y, X.test=X.test))
+  } else {
+    return(list(results=results, results.grf=results.grf,
+                EVT.par=EVT.par, X.train=X.train, Y=Y, X.test=X.test))
   }
+}
 
-  if(!grf)
-    return(list(results=results, EVT.par=EVT.par, X.train=X.train, Y=Y, X.test=X.test))
-  else
-    return(list(results=results, results.grf=results.grf, EVT.par=EVT.par, X.train=X.train, Y=Y, X.test=X.test))
+
+weighted.LLH <- function(data, weights, par) {
+  sig = par[1] # sigma
+  xi = par[2] # xi
+  y = 1 + (xi/sig) * data
+  if (min(sig) <= 0)
+    nl = 10^6
+  else {
+    if (min(y) <= 0)
+      nl = 10^6
+    else {
+      nl = sum(weights*(log(sig) + (1 + 1/xi)*log(y)))
+    }
+  }
+  return(nl)
+}
+
+q.GPD <- function(q, alpha, u, sigma, xi){
+  (((1-q)/(1-alpha))^{-xi} - 1)*sigma/xi + u
 }
