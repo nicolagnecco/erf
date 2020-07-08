c14f94548e5b2020e37d568da3533646ac62de41
Nicola <nicola.gnecco@gmail.com>
2020-07-08 15:59:53 +0200

    predict_erf

--- R/extremeForest.R
+++ R/extremeForest.R
@@ -8,8 +8,8 @@
 #'                  Extreme quantiles at which estimates are required.
 #' @param threshold Numeric (0, 1). Intermediate quantile used to compute
 #'                  thresholds \eqn{t(x_i)} for the GPD.
-#'                  Default is 0.8.
 #'                  Note that \code{threshold} < \code{quantiles}.
+#'                  Default is 0.8.
 #' @param newdata Numeric matrix.
 #'                Rows contain observation at which predictions should be made.
 #'                If NULL, makes out-of-bag predictions on the training set
@@ -30,7 +30,12 @@
 #'                   \eqn{t(x_i)} for the GPD?
 #'                   Default is \code{FALSE}.
 #'
-#' @return Predictions at each test point for each desired quantile.
+#' @return List. The list is made of:
+#' \enumerate{
+#' \item Numeric matrix. Predictions at each test point (on rows) for
+#'         each desired quantile (on columns).
+#' \item Plot if \code{model_assessment = TRUE}. QQ-plot for model assessment.
+#' }
 #'
 #' @export
 predict_erf <- function(object, quantiles, threshold = 0.8,
@@ -46,12 +51,18 @@ predict_erf <- function(object, quantiles, threshold = 0.8,
 
   t_xi <- compute_thresholds(object, threshold = threshold,
                              X = object$X.orig, out_of_bag = out_of_bag)
+  t_x0 <- compute_thresholds(object, threshold = threshold, X = X0)
 
   gpd_pars <- fit_conditional_gpd(object, wi_x0, t_xi)
 
-  q_hat <- compute_extreme_quantiles(gpd_pars, X0, quantiles, threshold)
+  q_hat <- compute_extreme_quantiles(gpd_pars, t_x0, quantiles, threshold)
 
-  if (model_assessment) compute_model_assessment(q_hat, newdata, Y.test)
+  if (model_assessment){
+    p <- compute_model_assessment(t_x0, Y.test, gpd_pars)
+    return(list(predictions = q_hat, plot = p))
+  } else {
+    return(list(predictions = q_hat))
+  }
 }
 
 validate_inputs <- function(object,  quantiles, threshold, newdata,
@@ -137,7 +148,7 @@ check_model_assessment <- function(model_assessment, newdata, Y.test){
 
     if(!class(Y.test) %in% valid.classes){
       stop(paste("Y.test must be one of the following classes:",
-           paste(valid.classes, collapse = ", "), sep = " "))
+                 paste(valid.classes, collapse = ", "), sep = " "))
     }
 
     n_ytest <- if(class(Y.test) == "numeric"){
@@ -169,7 +180,7 @@ set_test_observations <- function(object, newdata){
 }
 
 compute_thresholds <- function(object, threshold, X, out_of_bag = FALSE){
-  ## quantile_forest numeric(0, 1) numeric_matrix boolean -> numeric_vector
+  ## quantile_forest numeric(0, 1) numeric_matrix boolean -> numeric_matrix
   ## compute conditional quantile based on quantile_forest object
 
   if (out_of_bag){
@@ -201,15 +212,123 @@ fit_conditional_gpd <- function(object, wi_x0, t_xi){
   return(EVT_par)
 }
 
-compute_extreme_quantiles <- function(...){
-  # !!! write signature & purpose
+compute_extreme_quantiles <- function(gpd_pars, t_x0, quantiles, threshold){
+  ## numeric_matrix numeric_matrix numeric_vector(0, 1)
+  ## numeric(0, 1) -> numeric_matrix
+  ## produce matrix with estimated extremes quantiles. The value at (i, j) gives
+  ## the estimated quantile j for test sample i
+
+  res <- matrix(nrow = nrow(t_x0), ncol = length(quantiles))
+
+  for(j in seq_along(quantiles)){
+    res[, j] <- q_GPD(p = quantiles[j], p0 = threshold, t_x0 = t_x0,
+                      sigma = gpd_pars[, 1], xi = gpd_pars[, 2])
+  }
+
+  colnames(res) <- paste("quantile = ", quantiles)
+
+  return(res)
 }
 
-compute_model_assessment <- function(...){
-  # !!! write signature & purpose
+compute_model_assessment <- function(t_x0, Y.test, gpd_pars){
+  ## numeric_matrix numeric_vector numeric_matrix -> plot
+  ## produce QQ-plot of standardized estimated quantiles vs exponential data
+
+  Y <- Y.test
+  exc_idx = which(Y - t_x0 > 0)
+  exc_data = (Y - t_x0)[exc_idx]
+  xi <- gpd_pars[exc_idx, 2]
+  sigma <- gpd_pars[exc_idx, 1]
+
+  pseudo_obs <- compute_pseudo_observations(exc_data, sigma, xi)
+  n <- length(pseudo_obs)
+
+  observed_quantiles <- sort(pseudo_obs)
+  theoretical_quantiles <- stats::qexp((1:n)/ (n + 1))
+
+  plot_model_assessment(theoretical_quantiles, observed_quantiles)
+
+}
+
+compute_pseudo_observations <- function(exc_data, sigma, xi){
+  ## numeric_vector numeric numeric -> numeric_vector
+  ## compute pseudo observation on exponential margin
+
+  tmp_res <- 1 + xi * exc_data / sigma
+  idx <- which(tmp_res <= 0)
+
+  if (length(idx) == 1){
+    msg <- paste("Observation", idx, "is not plotted because",
+                 "it exceeds its upper end point")
+    warning(msg)
+  } else {
+    msg <- paste("Observations", paste(idx, collapse = ", "),
+                 "are not plotted because",
+                 "they exceed their upper end points")
+    warning(msg)
+  }
+
+  pseudo_obs <- 1 / xi[tmp_res > 0] * log(tmp_res[tmp_res > 0])
+  return(pseudo_obs)
+
+
+}
+
+plot_model_assessment <- function(x, y){
+  ## numeric_vector numeric_vector -> plot
+  ## produce QQ-plot
+
+  my_colors <- c("#0072B2", "#D55E00", "#CC79A7")
+
+  dat_plot <- data.frame(theoretical_quantiles = x,
+                         observed_quantiles = y)
+
+  dat_random <- sample_exponentials(dat_plot)
+
+  ggplot2::ggplot() +
+    ggplot2::geom_line(data = dat_random,
+                       ggplot2::aes(x = dat_random$theoretical_quantiles,
+                                    y = dat_random$random_exp,
+                                    group = rep), alpha = 0.2, col = "#D55E00") +
+    ggplot2::geom_abline(slope = 1, intercept = 0,
+                         linetype = "dashed", size = 1, col = "#0072B2") +
+    ggplot2::geom_point(data = dat_plot,
+                        ggplot2::aes(x = dat_plot$theoretical_quantiles,
+                                     y = dat_plot$observed_quantiles),
+                        size = 1) +
+
+    ggplot2::ggtitle("QQ-plot") +
+    ggplot2::xlab("theoretical quantiles") +
+    ggplot2::ylab("observed quantiles") +
+    ggplot2::theme_bw()
+
+}
+
+sample_exponentials <- function(dat_plot){
+  ## data_frame -> data_frame
+  ## produce data_frame with 100 randomly sampled exponential observations
+
+  n <- nrow(dat_plot)
+
+
+  dat <- data.frame(random_exp = double(), theoretical_quantiles = double(),
+                    rep = double())
+  for (i in 1:100) {
+    random_exp <- sort(stats::rexp(n = n))
+    theoretical_quantiles <- sort(dat_plot$theoretical_quantiles)
+
+    dat <- rbind(dat,
+                 data.frame(random_exp, theoretical_quantiles,
+                            rep = rep(i, n)))
+  }
+
+  return(dat)
 }
 
 weighted_LLH <- function(data, weights, par) {
+  ## numeric_matrix numeric_vector numeric_vector -> numeric
+  ## returns the weighted GPD log-likelihood
+
   sig = par[1] # sigma
   xi = par[2] # xi
   y = 1 + (xi/sig) * data
@@ -225,98 +344,10 @@ weighted_LLH <- function(data, weights, par) {
   return(nl)
 }
 
-q_GPD <- function(q, alpha, u, sigma, xi){
-  (((1-q)/(1-alpha))^{-xi} - 1)*sigma/xi + u
+q_GPD <- function(p, p0, t_x0, sigma, xi){
+  ## numeric(0, 1) numeric(0, 1) numeric_matrix numeric_vector
+  ## numeric_vector -> numeric_vector
+  ## produce the estimated extreme quantiles for the test samples
+
+  (((1-p)/(1-p0))^{-xi} - 1) * (sigma / xi) + t_x0
 }
-
-
-# draft ###########
-
-draft <- function(X.train, Y, X.test, alpha, alpha.new, Y.test=NULL, grf=FALSE,
-                  min.node.size = 5, q.hat.train = 0){
-
-  ntest <- nrow(X.test)
-  results <- array(NA, dim = c(ntest, length(alpha.new)))
-  EVT.par <- array(NA, dim = c(ntest, 2))
-
-  fit.grf = grf::quantile_forest(X.train, Y, quantiles = c(0.1,0.5,0.9),
-                                 min.node.size = min.node.size, num.trees = 2, honesty = FALSE) #c(0.7, 0.8, alpha, 0.95)
-  q.hat.train2 = stats::predict(fit.grf, X.train, quantiles = alpha)
-  q.hat.train2 = stats::predict(fit.grf, quantiles = alpha)
-  exc.idx = which(Y - q.hat.train > 0)
-  exc.data = (Y - q.hat.train)[exc.idx]
-
-
-  # Estimate GRF and EVT quantiles
-  if (grf){
-    results.grf <- stats::predict(fit.grf, X.test, quantiles = alpha.new)
-  }
-
-
-
-  # Obtaining weights (dimension is ntest x n)
-  qrf.weights = grf::get_sample_weights(fit.grf, newdata = X.test,
-                                        num.threads = NULL)
-
-
-  #plot(X.train[,1],qrf.weights[24,])
-
-  in.par <- ismev::gpd.fit(exc.data, 0, show=FALSE)$mle
-  for(i in 1:ntest){
-    EVT.par[i,] = stats::optim(par=in.par, fn=weighted.LLH, data=exc.data,
-                               weights=qrf.weights[i,exc.idx])$par
-  }
-  #plot(X.test[,1],EVT.par[,1,r])
-
-  # Estimate GRF and EVT quantiles
-  q.alpha.test = stats::predict(fit.grf, X.test, quantiles = alpha)
-  for(j in 1:length(alpha.new)){
-    results[,j] <- q.GPD(q = alpha.new[j], alpha=alpha,
-                         u = q.alpha.test, sigma = EVT.par[,1], xi = EVT.par[,2])
-  }
-
-  # if(!is.null(Y.test)){
-  #   par(mfrow=c(1,3))
-  #
-  #   ## plot qq plot for unconditional model, ignoring covariates
-  #   q.hat.uncon <- quantile(Y, probs = alpha)
-  #   exc.uncon.idx = which(Y - q.hat.uncon > 0)
-  #   exc.uncon = (Y - q.hat.uncon)[exc.uncon.idx]
-  #   uncon.par <- gpd.fit(exc.uncon, 0, show=FALSE)$mle
-  #
-  #   exc.test.uncon.idx = which(Y.test -  q.hat.uncon > 0)
-  #   exc.test.uncon = (Y.test - q.hat.uncon)[exc.test.uncon.idx]
-  #   n.exc.test.uncon = length(exc.test.uncon)
-  #   exc.test.uncon.std <- 1/uncon.par[2] * log(1+uncon.par[2] * exc.test.uncon/uncon.par[1])
-  #   plot(qexp((1:n.exc.test.uncon)/ (n.exc.test.uncon+1)), sort(exc.test.uncon.std))
-  #   abline(0,1)
-  #
-  #   ## plot qq plot for model that uses covariates only for the alpha-threshold
-  #   exc.test.idx = which(Y.test -  q.alpha.test > 0)
-  #   exc.test = (Y.test - q.alpha.test)[exc.test.idx]
-  #   n.exc.test = length(exc.test)
-  #
-  #   exc.test.std2 <- 1/in.par[2] * log(1+in.par[2] * exc.test/in.par[1])
-  #   plot(qexp((1:n.exc.test)/ (n.exc.test+1)), sort(exc.test.std2))
-  #   abline(0,1)
-  #
-  #
-  #   ## plot qq plot for full covariate model for alpha-threshold and GPD parameters
-  #   exc.test.std <- 1/EVT.par[exc.test.idx,2] * log(1+EVT.par[exc.test.idx,2] * exc.test/EVT.par[exc.test.idx,1])
-  #   plot(qexp((1:n.exc.test)/ (n.exc.test+1)), sort(exc.test.std))
-  # abline(0,1)
-  #
-  #
-  #
-  # }
-
-  if (!grf){
-    return(list(results=results, EVT.par=EVT.par, X.train=X.train,
-                Y=Y, X.test=X.test))
-  } else {
-    return(list(results=results, results.grf=results.grf,
-                EVT.par=EVT.par, X.train=X.train, Y=Y, X.test=X.test))
-  }
-}
-
-
--- analysis/understandgrf.R
+++ analysis/understandgrf.R
@@ -1,7 +1,7 @@
 library(grf)
 library(tidyverse)
 
-# Train a quantile forest.
+# Understand how grf::quantile_forest works
 n <- 50
 p <- 3
 X <- matrix(rnorm(n * p), n, p)
@@ -46,8 +46,8 @@ W[, 35]
 
 
 # Test
-n <- 50
-p <- 3
+n <- 500
+p <- 10
 X <- matrix(rnorm(n * p), n, p)
 Y <- X[, 1] * rnorm(n)
 object <- quantile_forest(X, Y, quantiles = c(0.1, 0.5, 0.9))
@@ -56,17 +56,121 @@ newdata <-  matrix(rnorm(n * p), n, p)
 # newdata <-  matrix(rnorm(n * (p - 1)), n, p - 1)
 # newdata <- NULL
 quantiles <- c(.99, .999)
-threshold <- 0.85
+threshold <- 0.5
 model_assessment <- TRUE
 Y.test <- newdata[, 1] * rnorm(n)
+out_of_bag <- FALSE
 # Y.test <- NULL
 # Y.test <- newdata[-1, 1] * rnorm(n - 1)
-validate_inputs(object, quantiles, threshold, newdata, model_assessment, Y.test)
+res <- predict_erf(object, quantiles = quantiles, threshold = threshold,
+                   newdata = newdata, model_assessment = TRUE,
+                   Y.test = Y.test, out_of_bag = FALSE)
 
-predict_erf(object, quantiles, threshold, newdata, model_assessment, Y.test,
-            FALSE)
+res$predictions
+res$plot
 
+# compare with original code
 X.train <- X
 X.test <- newdata
 alpha <- threshold
 alpha.new <- quantiles
+min.node.size <- 5
+fit.grf <- object
+
+
+weighted.LLH = function(data, weights, par) {
+  sig = par[1] # sigma
+  xi = par[2] # xi
+  y = 1 + (xi/sig) * data
+  if (min(sig) <= 0)
+    nl = 10^6
+  else {
+    if (min(y) <= 0)
+      nl = 10^6
+    else {
+      nl = sum(weights*(log(sig) + (1 + 1/xi)*log(y)))
+    }
+  }
+  return(nl)
+}
+
+q.GPD = function(q, alpha, u, sigma, xi){
+  (((1-q)/(1-alpha))^{-xi} - 1)*sigma/xi + u
+}
+
+extreme_forest <- function(X.train, Y, X.test, alpha, alpha.new, Y.test=NULL, grf=FALSE){
+
+  ntest <- nrow(X.test)
+  results <- array(NA, dim = c(ntest, length(alpha.new)))
+  EVT.par <- array(NA, dim = c(ntest, 2))
+
+  fit.grf = quantile_forest(X.train, Y, quantiles = c(0.1,0.5,0.9), min.node.size = min.node.size) #c(0.7, 0.8, alpha, 0.95)
+  q.hat.train = predict(fit.grf, X.train, quantiles = alpha)
+  exc.idx = which(Y - q.hat.train > 0)
+  exc.data = (Y - q.hat.train)[exc.idx]
+
+
+  # Estimate GRF and EVT quantiles
+  if(grf) results.grf <- predict(fit.grf, X.test, quantiles = alpha.new)
+
+
+
+  # Obtaining weights (dimension is ntest x n)
+  qrf.weights = get_sample_weights(fit.grf, newdata = X.test, num.threads = NULL)
+
+
+  #plot(X.train[,1],qrf.weights[24,])
+
+  in.par <- ismev::gpd.fit(exc.data, 0, show=FALSE)$mle
+  for(i in 1:ntest){
+    EVT.par[i,] = optim(par=in.par, fn=weighted.LLH, data=exc.data, weights=qrf.weights[i,exc.idx])$par
+  }
+  #plot(X.test[,1],EVT.par[,1,r])
+
+  # Estimate GRF and EVT quantiles
+  q.alpha.test = predict(fit.grf, X.test, quantiles = alpha)
+  for(j in 1:length(alpha.new)){
+    results[,j] <- q.GPD(q = alpha.new[j], alpha=alpha,u  = q.alpha.test, sigma = EVT.par[,1], xi = EVT.par[,2])
+  }
+
+  if(!is.null(Y.test)){
+    par(mfrow=c(1,3))
+
+    ## plot qq plot for unconditional model, ignoring covariates
+    q.hat.uncon <- quantile(Y, probs = alpha)
+    exc.uncon.idx = which(Y - q.hat.uncon > 0)
+    exc.uncon = (Y - q.hat.uncon)[exc.uncon.idx]
+    uncon.par <- gpd.fit(exc.uncon, 0, show=FALSE)$mle
+
+    exc.test.uncon.idx = which(Y.test -  q.hat.uncon > 0)
+    exc.test.uncon = (Y.test - q.hat.uncon)[exc.test.uncon.idx]
+    n.exc.test.uncon = length(exc.test.uncon)
+    exc.test.uncon.std <- 1/uncon.par[2] * log(1+uncon.par[2] * exc.test.uncon/uncon.par[1])
+    plot(qexp((1:n.exc.test.uncon)/ (n.exc.test.uncon+1)), sort(exc.test.uncon.std))
+    abline(0,1)
+
+    ## plot qq plot for model that uses covariates only for the alpha-threshold
+    exc.test.idx = which(Y.test -  q.alpha.test > 0)
+    exc.test = (Y.test - q.alpha.test)[exc.test.idx]
+    n.exc.test = length(exc.test)
+
+    exc.test.std2 <- 1/in.par[2] * log(1+in.par[2] * exc.test/in.par[1])
+    plot(qexp((1:n.exc.test)/ (n.exc.test+1)), sort(exc.test.std2))
+    abline(0,1)
+
+
+    ## plot qq plot for full covariate model for alpha-threshold and GPD parameters
+    exc.test.std <- 1/EVT.par[exc.test.idx,2] * log(1+EVT.par[exc.test.idx,2] * exc.test/EVT.par[exc.test.idx,1])
+    plot(qexp((1:(n.exc.test-1))/ (n.exc.test)), sort(exc.test.std))
+    abline(0,1)
+
+
+
+  }
+
+  if(!grf)
+    return(list(results=results, EVT.par=EVT.par, X.train=X.train, Y=Y, X.test=X.test))
+  else
+    return(list(results=results, results.grf=results.grf, EVT.par=EVT.par, X.train=X.train, Y=Y, X.test=X.test))
+}
+
