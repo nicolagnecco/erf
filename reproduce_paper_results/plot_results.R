9b5e4e5e57dfd7573ea6061c7d88be4f572e313a
Nicola <nicola.gnecco@gmail.com>
2020-09-10 00:09:14 +0200

    plot results

--- reproduce_paper_results/plot_results.R
+++ reproduce_paper_results/plot_results.R
@@ -2,8 +2,8 @@ rm(list = ls())
 library(tidyverse)
 source("reproduce_paper_results/simulation_functions.R")
 
-# plot results
-dat <- read_rds("reproduce_paper_results/output/simulations2.rds")
+# plot results ####
+dat <- read_rds("reproduce_paper_results/output/simulations.rds")
 
 n0 <- 2e3
 p0 <- 40
@@ -14,7 +14,8 @@ honesty0 <- TRUE
 threshold0 <- 0.8
 out_of_bag0 <- FALSE
 
-aa <- dat %>%
+## sample size ####
+dat_plot <- dat %>%
   filter(p %in% p0 , scale %in% scale0 ,
          num.trees %in% num.trees0 , min.node.size %in% min.node.size0 ,
          honesty %in% honesty0 , threshold %in% threshold0 ,
@@ -38,45 +39,304 @@ aa <- dat %>%
   mutate(method = factor(method))
 
 
-ggplot(aa, aes(x = n, y = rimse, col = method)) +
+gg <- ggplot(dat_plot %>% filter(quantiles_predict >= .99), aes(x = n, y = rimse, col = method)) +
+  facet_grid(model ~ quantiles_predict, scale = "free") +
+  geom_line(size = 1, alpha = .5) +
+  geom_point(size = 3) +
+  scale_color_manual(values = c("#D55E00","#0072B2", "#CC79A7"))
+
+ggsave(paste("reproduce_paper_results/output/simulation_", "n", ".pdf", sep = ""),
+       gg, width = 10, height = 7.5, units = c("in"))
+
+
+
+## dimension ####
+dat_plot <- dat %>%
+  filter(n %in% n0 , scale %in% scale0 ,
+         num.trees %in% num.trees0 , min.node.size %in% min.node.size0 ,
+         honesty %in% honesty0 , threshold %in% threshold0 ,
+         out_of_bag %in% out_of_bag0) %>%
+  mutate(method = factor(method),
+         # training_id = factor(training_id),
+         model = if_else(model == "gaussian", model,
+                         paste(model, "_", df, sep = ""))) %>%
+  select(nexp, method, model, quantiles_predict, predictions, p, x) %>%
+  unnest(cols = c(quantiles_predict, predictions)) %>%
+  pivot_wider(names_from = "method",
+              values_from = "predictions") %>%
+  mutate(grf = (grf - true)^2,
+         erf = (erf - true)^2) %>%
+  select(-true) %>%
+  pivot_longer(cols = c("grf", "erf"),
+               names_to = "method", values_to = "se") %>%
+  group_by(model, quantiles_predict, p, method, x) %>%
+  summarise(mse = mean(se)) %>%     # mean across nexp
+  summarise(rimse = sqrt(mean(mse)))  %>%  # mean across x's
+  mutate(method = factor(method))
+
+
+gg <- ggplot(dat_plot %>% filter(quantiles_predict >= .99), aes(x = p, y = rimse, col = method)) +
   facet_grid(model ~ quantiles_predict, scale = "free") +
   geom_line(size = 1, alpha = .5) +
   geom_point(size = 3) +
   scale_color_manual(values = c("#D55E00","#0072B2", "#CC79A7"))
 
 
-models <- unique(dat$model)
-i <- 1
+ggsave(paste("reproduce_paper_results/output/simulation_", "p", ".pdf", sep = ""),
+       gg, width = 10, height = 7.5, units = c("in"))
+
+## scale ####
+dat_plot <- dat %>%
+  filter(n %in% n0 , p %in% p0,
+         num.trees %in% num.trees0 , min.node.size %in% min.node.size0 ,
+         honesty %in% honesty0 , threshold %in% threshold0 ,
+         out_of_bag %in% out_of_bag0) %>%
+  mutate(method = factor(method),
+         # training_id = factor(training_id),
+         model = if_else(model == "gaussian", model,
+                         paste(model, "_", df, sep = ""))) %>%
+  select(nexp, method, model, quantiles_predict, predictions, scale, x) %>%
+  unnest(cols = c(quantiles_predict, predictions)) %>%
+  pivot_wider(names_from = "method",
+              values_from = "predictions") %>%
+  mutate(grf = (grf - true)^2,
+         erf = (erf - true)^2) %>%
+  select(-true) %>%
+  pivot_longer(cols = c("grf", "erf"),
+               names_to = "method", values_to = "se") %>%
+  group_by(model, quantiles_predict, scale, method, x) %>%
+  summarise(mse = mean(se)) %>%     # mean across nexp
+  summarise(rimse = sqrt(mean(mse)))  %>%  # mean across x's
+  mutate(method = factor(method))
 
 
-for (i in seq_along(models)){
-  m <- models[i]
+gg <- ggplot(dat_plot %>% filter(quantiles_predict >= .99),
+       aes(x = scale, y = rimse, col = method)) +
+  facet_grid(model ~ quantiles_predict, scale = "free") +
+  geom_line(size = 1, alpha = .5) +
+  geom_point(size = 3) +
+  scale_color_manual(values = c("#D55E00","#0072B2", "#CC79A7"))
 
-  dat_plot <- dat %>%
-    filter(model == m)
 
-  dat_methods <- dat_plot %>%
-    filter(method != "true")
+ggsave(paste("reproduce_paper_results/output/simulation_", "scale", ".pdf", sep = ""),
+       gg, width = 10, height = 7.5, units = c("in"))
 
-  dat_quantile_function <- dat_plot %>%
-    filter(method == "true")
 
-  gg <- ggplot() +
-    geom_ribbon(data = dat_methods,
-                aes(x = x,
-                    ymin = lb, ymax = ub,
-                    fill = method),
-                alpha = .25) +
-    geom_line(data = dat_methods,
-              aes(x = x, y = mean_quantile, color = method),
-              alpha = .8, size = 1) +
-    geom_step(data = dat_quantile_function,
-              aes(x = x, y = mean_quantile)) +
-    scale_fill_manual(values = c("#D55E00","#0072B2", "#CC79A7")) +
-    scale_color_manual(values = c("#D55E00","#0072B2", "#CC79A7")) +
-    facet_grid(rows = vars(alpha), scale = "free")
+## num.trees ####
+dat_plot <- dat %>%
+  filter(n %in% n0 , p %in% p0, scale %in% scale0 ,
+         min.node.size %in% min.node.size0 ,
+         honesty %in% honesty0 , threshold %in% threshold0 ,
+         out_of_bag %in% out_of_bag0) %>%
+  mutate(method = factor(method),
+         # training_id = factor(training_id),
+         model = if_else(model == "gaussian", model,
+                         paste(model, "_", df, sep = ""))) %>%
+  select(nexp, method, model, quantiles_predict, predictions, num.trees, x) %>%
+  unnest(cols = c(quantiles_predict, predictions)) %>%
+  pivot_wider(names_from = "method",
+              values_from = "predictions") %>%
+  mutate(grf = (grf - true)^2,
+         erf = (erf - true)^2) %>%
+  select(-true) %>%
+  pivot_longer(cols = c("grf", "erf"),
+               names_to = "method", values_to = "se") %>%
+  group_by(model, quantiles_predict, num.trees, method, x) %>%
+  summarise(mse = mean(se)) %>%     # mean across nexp
+  summarise(rimse = sqrt(mean(mse)))  %>%  # mean across x's
+  mutate(method = factor(method))
 
-  ggsave(paste("reproduce_paper_results/output/simulation_", m, ".pdf", sep = ""),
-         gg, width = 10, height = 7.5, units = c("in"))
 
-}
+gg <- ggplot(dat_plot %>% filter(quantiles_predict >= .99), aes(x = num.trees, y = rimse, col = method)) +
+  facet_grid(model ~ quantiles_predict, scale = "free") +
+  geom_line(size = 1, alpha = .5) +
+  geom_point(size = 3) +
+  scale_color_manual(values = c("#D55E00","#0072B2", "#CC79A7"))
+
+
+ggsave(paste("reproduce_paper_results/output/simulation_", "numtrees", ".pdf", sep = ""),
+       gg, width = 10, height = 7.5, units = c("in"))
+
+## min.node.size ####
+dat_plot <- dat %>%
+  filter(n %in% n0 , p %in% p0, scale %in% scale0 ,
+         num.trees %in% num.trees0,
+         honesty %in% honesty0 , threshold %in% threshold0 ,
+         out_of_bag %in% out_of_bag0) %>%
+  mutate(method = factor(method),
+         # training_id = factor(training_id),
+         model = if_else(model == "gaussian", model,
+                         paste(model, "_", df, sep = ""))) %>%
+  select(nexp, method, model, quantiles_predict, predictions, min.node.size, x) %>%
+  unnest(cols = c(quantiles_predict, predictions)) %>%
+  pivot_wider(names_from = "method",
+              values_from = "predictions") %>%
+  mutate(grf = (grf - true)^2,
+         erf = (erf - true)^2) %>%
+  select(-true) %>%
+  pivot_longer(cols = c("grf", "erf"),
+               names_to = "method", values_to = "se") %>%
+  group_by(model, quantiles_predict, min.node.size, method, x) %>%
+  summarise(mse = mean(se)) %>%     # mean across nexp
+  summarise(rimse = sqrt(mean(mse)))  %>%  # mean across x's
+  mutate(method = factor(method))
+
+
+gg <- ggplot(dat_plot %>% filter(quantiles_predict >= .99), aes(x = min.node.size, y = rimse, col = method)) +
+  facet_grid(model ~ quantiles_predict, scale = "free") +
+  geom_line(size = 1, alpha = .5) +
+  geom_point(size = 3) +
+  scale_color_manual(values = c("#D55E00","#0072B2", "#CC79A7"))
+
+ggsave(paste("reproduce_paper_results/output/simulation_", "minnodesize", ".pdf", sep = ""),
+       gg, width = 10, height = 7.5, units = c("in"))
+
+
+
+
+## honesty ####
+dat_plot <- dat %>%
+  filter(n %in% n0 , p %in% p0, scale %in% scale0 ,
+         num.trees %in% num.trees0, min.node.size %in% min.node.size0,
+         threshold %in% threshold0 ,
+         out_of_bag %in% out_of_bag0) %>%
+  mutate(method = factor(method),
+         # training_id = factor(training_id),
+         model = if_else(model == "gaussian", model,
+                         paste(model, "_", df, sep = ""))) %>%
+  select(nexp, method, model, quantiles_predict, predictions, honesty, x) %>%
+  unnest(cols = c(quantiles_predict, predictions)) %>%
+  pivot_wider(names_from = "method",
+              values_from = "predictions") %>%
+  mutate(grf = (grf - true)^2,
+         erf = (erf - true)^2) %>%
+  select(-true) %>%
+  pivot_longer(cols = c("grf", "erf"),
+               names_to = "method", values_to = "se") %>%
+  group_by(model, quantiles_predict, honesty, method, x) %>%
+  summarise(mse = mean(se)) %>%     # mean across nexp
+  summarise(rimse = sqrt(mean(mse)))  %>%  # mean across x's
+  mutate(method = factor(method),
+         honesty = as.numeric(honesty))
+
+
+gg <- ggplot(dat_plot %>% filter(quantiles_predict >= .99), aes(x = honesty, y = rimse, col = method)) +
+  facet_grid(model ~ quantiles_predict, scale = "free") +
+  geom_line(size = 1, alpha = .5) +
+  geom_point(size = 3) +
+  scale_color_manual(values = c("#D55E00","#0072B2", "#CC79A7"))
+
+ggsave(paste("reproduce_paper_results/output/simulation_", "honesty", ".pdf", sep = ""),
+       gg, width = 10, height = 7.5, units = c("in"))
+
+## threshold ####
+dat_plot <- dat %>%
+  filter(n %in% n0 , p %in% p0, scale %in% scale0 ,
+         num.trees %in% num.trees0, min.node.size %in% min.node.size0,
+         honesty %in% honesty0,
+         out_of_bag %in% out_of_bag0) %>%
+  mutate(method = factor(method),
+         # training_id = factor(training_id),
+         model = if_else(model == "gaussian", model,
+                         paste(model, "_", df, sep = ""))) %>%
+  select(nexp, method, model, quantiles_predict, predictions, threshold, x) %>%
+  unnest(cols = c(quantiles_predict, predictions)) %>%
+  pivot_wider(names_from = "method",
+              values_from = "predictions") %>%
+  mutate(grf = (grf - true)^2,
+         erf = (erf - true)^2) %>%
+  select(-true) %>%
+  pivot_longer(cols = c("grf", "erf"),
+               names_to = "method", values_to = "se") %>%
+  group_by(model, quantiles_predict, threshold, method, x) %>%
+  summarise(mse = mean(se)) %>%     # mean across nexp
+  summarise(rimse = sqrt(mean(mse)))  %>%  # mean across x's
+  mutate(method = factor(method))
+
+
+gg <- ggplot(dat_plot %>% filter(quantiles_predict >= .99), aes(x = threshold, y = rimse, col = method)) +
+  facet_grid(model ~ quantiles_predict, scale = "free") +
+  geom_line(size = 1, alpha = .5) +
+  geom_point(size = 3) +
+  scale_color_manual(values = c("#D55E00","#0072B2", "#CC79A7"))
+
+
+
+
+ggsave(paste("reproduce_paper_results/output/simulation_", "threshold", ".pdf", sep = ""),
+       gg, width = 10, height = 7.5, units = c("in"))
+
+
+
+## out_of_bag ####
+dat_plot <- dat %>%
+  filter(n %in% n0 , p %in% p0, scale %in% scale0 ,
+         num.trees %in% num.trees0, min.node.size %in% min.node.size0,
+         honesty %in% honesty0 , threshold %in% threshold0) %>%
+  mutate(method = factor(method),
+         # training_id = factor(training_id),
+         model = if_else(model == "gaussian", model,
+                         paste(model, "_", df, sep = ""))) %>%
+  select(nexp, method, model, quantiles_predict, predictions, out_of_bag, x) %>%
+  unnest(cols = c(quantiles_predict, predictions)) %>%
+  pivot_wider(names_from = "method",
+              values_from = "predictions") %>%
+  mutate(grf = (grf - true)^2,
+         erf = (erf - true)^2) %>%
+  select(-true) %>%
+  pivot_longer(cols = c("grf", "erf"),
+               names_to = "method", values_to = "se") %>%
+  group_by(model, quantiles_predict, out_of_bag, method, x) %>%
+  summarise(mse = mean(se)) %>%     # mean across nexp
+  summarise(rimse = sqrt(mean(mse)))  %>%  # mean across x's
+  mutate(method = factor(method),
+         out_of_bag = as.numeric(out_of_bag))
+
+
+gg <- ggplot(dat_plot %>% filter(quantiles_predict >= .99), aes(x = out_of_bag, y = rimse, col = method)) +
+  facet_grid(model ~ quantiles_predict, scale = "free") +
+  geom_line(size = 1, alpha = .5) +
+  geom_point(size = 3) +
+  scale_color_manual(values = c("#D55E00","#0072B2", "#CC79A7"))
+
+ggsave(paste("reproduce_paper_results/output/simulation_", "outofbag", ".pdf", sep = ""),
+       gg, width = 10, height = 7.5, units = c("in"))
+
+
+
+# #####
+# models <- unique(dat$model)
+# i <- 1
+#
+#
+# for (i in seq_along(models)){
+#   m <- models[i]
+#
+#   dat_plot <- dat %>%
+#     filter(model == m)
+#
+#   dat_methods <- dat_plot %>%
+#     filter(method != "true")
+#
+#   dat_quantile_function <- dat_plot %>%
+#     filter(method == "true")
+#
+#   gg <- ggplot() +
+#     geom_ribbon(data = dat_methods,
+#                 aes(x = x,
+#                     ymin = lb, ymax = ub,
+#                     fill = method),
+#                 alpha = .25) +
+#     geom_line(data = dat_methods,
+#               aes(x = x, y = mean_quantile, color = method),
+#               alpha = .8, size = 1) +
+#     geom_step(data = dat_quantile_function,
+#               aes(x = x, y = mean_quantile)) +
+#     scale_fill_manual(values = c("#D55E00","#0072B2", "#CC79A7")) +
+#     scale_color_manual(values = c("#D55E00","#0072B2", "#CC79A7")) +
+#     facet_grid(rows = vars(alpha), scale = "free")
+#
+#   ggsave(paste("reproduce_paper_results/output/simulation_", m, ".pdf", sep = ""),
+#          gg, width = 10, height = 7.5, units = c("in"))
+#
+# }
