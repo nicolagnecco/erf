996f306c0175aad45435667d4d40c4b232273433
Nicola Gnecco <nicola.gnecco@gmail.com>
2021-02-15 15:09:47 +0100

    Update constants and data definition

--- tests/testthat/helper-const_data_defs.R
+++ tests/testthat/helper-const_data_defs.R
@@ -1,4 +1,4 @@
-## Constants:
+#' Constants:
 
 n <- 1e3
 p <- 10
@@ -9,58 +9,62 @@ df <- 4
 
 
 
-## Data definitions:
+#' Data definitions:
 
-## X is numeric_matrix with n rows and p columns
-## interp. matrix with n observations with p predictors
+#' `X` is `numeric_matrix` with `n` rows and `p` columns
+#' interp. matrix with `n` observations with `p` predictors
 X <- matrix(runif(n * p, min = -1, max = 1), nrow = n, ncol = p)
 X_test <- matrix(runif(n_test * p, min = -1, max = 1), nrow = n_test, ncol = p)
 
 
-## Y is numeric_vector with n elements
-## interp. vector of responses
+#' `Y` is `numeric_vector` with `n` elements
+#' interp. vector of responses
 Y <- (1 + 1 * (X[, 1] > 0)) * rt(n, df)
 Y_test <- (1 + 1 * (X_test[, 1] > 0)) * rt(n_test, df)
 
 
-## quantile_forest is an object from grf::quantile
-## interp. a quantile forest
+#' `quantile_forest` is an object from `grf::quantile`
+#' interp. a quantile forest
 quantile_forest_1 <- grf::quantile_forest(X, Y, min.node.size = min.node.size)
 
 
-## intermediate_estimator is object of any class such that
-## - it has a predict method
-## - the second argument of such predict is the numeric_matrix of test predictors
-## interp. a fitted object to estimate intermediate quantile
-intermediate_estimator_1 <- grf::quantile_forest(X, Y)
-intermediate_estimator_2 <- structure(list(), class = "foo")
-intermediate_estimator_3 <- structure(list(), class = "lm")
-intermediate_estimator_4 <- lm(Y ~ X)
+#' `intermediate_estimator` is one of:
+#' - `"grf"`
+#' - `"neural_nets"`
+#' interp. type of estimator used to fit the intermediate threshold.
+
+
+#' `intermediate_threshold` is one of
+#' - `"quantile_forest"`, if `intermediate_threshold = "grf"`.
+#' - `"..."`, if `intermediate_threshold = "neural_nets"`.
+#' interp. a fitted object to predict intermediate thresholds.
+intermediate_threshold_1 <- grf::quantile_forest(X, Y)
+intermediate_threshold_2 <- structure(list(), class = "quantile_forest")
 
 
 structure(list(), class = "extreme_forest")
-##' `extreme_forest` is a named list made of:
-##' * `quantile_forest` is `quantile_forest`.
-##' * `intermediate_estimator` is `intermediate_estimator`.
-##' * `min.node.size` is numeric.
-##' * `lambda` is numeric.
-##' interp. an extreme forest
+#' `extreme_forest` is a named list made of:
+#' - `quantile_forest` is `quantile_forest`.
+#' - `min.node.size` is numeric.
+#' - `lambda` is numeric.
+#' - `intermediate_threshold` is `intermediate_threshold`.
+#' interp. an extreme forest.
 extreme_forest_1 <- structure(list(
   "quantile_forest" = quantile_forest_1,
-  "intermediate_estimator" = intermediate_estimator_1,
   "min.node.size" = min.node.size,
-  "lambda" = lambda
+  "lambda" = lambda,
+  "intermediate_threshold" = intermediate_threshold_1,
 ),
 class = "extreme_forest"
 )
 
 
 structure(list(), class = "extreme_forest_cv")
-##' `extreme_forest_cv` is a named list made of:
-##' * `scores`: a `tibble` with columns: `min.node.size`, `lambda`, `cvm`
-##'  (mean cross-validated error).
-##' * `extreme_forest.fit`: a fitted "`extreme_forest`" object on the full data.
-##' interp. a cross-validated extreme forest
+#'' `extreme_forest_cv` is a named list made of:
+#'' * `scores`: a `tibble` with columns: `min.node.size`, `lambda`, `cvm`
+#''  (mean cross-validated error).
+#'' * `extreme_forest.fit`: a fitted "`extreme_forest`" object on the full data.
+#'' interp. a cross-validated extreme forest.
 extreme_forest_cv_1 <- list(
   "scores" = tibble::tibble(
     min.node.size = c(5, 40),
