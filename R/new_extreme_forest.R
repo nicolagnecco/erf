3da9316639180dd0526fd0cf41269c7f2b35c3c3
Nicola Gnecco <nicola.gnecco@gmail.com>
2021-02-15 17:40:21 +0100

    Document new_extreme_forest fun

--- R/new_extreme_forest.R
+++ R/new_extreme_forest.R
@@ -1,12 +1,58 @@
-
-
-
+#' Fit an extremal random forest (ERF)
+#'
+#'
+#' Internal function to fit an extremal random forest ... !!! write description
+#'
+#'
+#' !!! write details
+#'
+#'
+#' @param X Numeric matrix (or `data.frame`) of predictors, where each row
+#'  corresponds to an observation and each column to a predictor.
+#'
+#' @param Y Numeric vector of responses.
+#'
+#' @param min.node.size Minimum number of observations in each tree leaf used
+#'  in [grf::quantile_forest()].
+#'  Nodes with size smaller than `min.node.size` can occur,
+#'  as in the original \pkg{randomForest} package.
+#'  Default is 5.
+#'
+#' @param lambda Penalty for the shape parameter used in the weighted likelihood.
+#'  Default is 0.001.
+#'
+#' @param intermediate_threshold An object with arbitrary S3 class that
+#'  possesses a `predict` method.
+#'  Such "`predict`" method must accept a numeric matrix (or `data.frame`)
+#'  of test predictors, e.g., `newdata`, as **second** argument.
+#'  For example, `predict(intermediate_threshold, newdata, ...)`
+#'  must execute with no errors.
+#'
+#'  This is a fitted object used to predict the intermediate thresholds.
+#'
+#'
+#' @return An object with S3 class "`extreme_forest`".
+#'  It is a named list with the following elements:
+#'
+#'  \item{quantile_forest}{An object with S3 class "`quantile_forest`" (see
+#'    [grf::quantile_forest()]). This is a fitted generalized random forest that
+#'    contains the similarity weights used in the log-likelihood.}
+#'
+#'  \item{min.node.size}{Minimum number of observations in each tree leaf used
+#'    to fit the `quantile_forest`.}
+#'
+#'  \item{lambda}{Penalty for the shape parameter used in the weighted
+#'   log-likelihood.}
+#'
+#'  \item{intermediate_threshold}{An object with arbitraty S3 class, as
+#'   described in the **Arguments** section.
+#'   It is a fitted object used to predict the intermediate thresholds.}
+#'
+#'
+#' @examples
+#' "!!! add examples"
 new_extreme_forest <- function(X, Y, min.node.size, lambda,
-                               intermediate_estimator) {
-  ## numeric_matrix numeric_vector numeric numeric intermediate_estimator
-  ## -> extreme_forest
-  ## fits an extreme_forest
-
+                               intermediate_threshold) {
   # !!!
   # examples
   # template / inventory
@@ -21,10 +67,10 @@ new_extreme_forest <- function(X, Y, min.node.size, lambda,
 
   # return extreme_forest object
   structure(list(
-    "quantile_forest" = structure(list(), class = "quantile_forest"),
+    "quantile_forest" = extreme_quantile_fit,
     min.node.size = min.node.size,
     lambda = lambda,
-    "intermediate_estimator" = structure(list(), class = "quantile_forest")
+    "intermediate_threshold" = intermediate_threshold
   ),
   class = "extreme_forest"
   )
